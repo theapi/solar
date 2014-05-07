1ae2d479f18769de613d4714bf5c3eebff4b3906
Peter <petermclarke@gmail.com>
2014-05-07 13:54:23 +0100

    Note about ServoTimer2

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -14,6 +14,13 @@ Need to try in sunlight to get better idea of good resistor value.
 ----------------------------------------------------
 */
 
+#include <util/delay.h>
+#include <avr/wdt.h>
+//#include <VirtualWire.h>
+// Investigate replacing Servo with ServoTimer2
+// So that virtual wire can be used.
+// virtualwire & servo both use timer1
+// http://forum.arduino.cc/index.php?PHPSESSID=chto8kajoid84icno5e5sm0jk7&topic=21975.0
 #include <Servo.h> 
 
 #define PIN_LDR_UP    0 // PC0
@@ -140,5 +147,31 @@ int tkr_diff_horz()
     }
     
     return 0;
+}
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  _delay_ms(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
 }
 
