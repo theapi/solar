304c858d652cbb43dfc2acf2c77d6efc8e269462
theapi <petermclarke@gmail.com>
2014-05-13 20:17:23 +0100

    Clear watchdog interupt as soon as possible

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -16,9 +16,9 @@ Need to try in sunlight to get better idea of good resistor value.
 
 #include <util/delay.h>
 #include <avr/wdt.h>
-//#include <VirtualWire.h>
+
 #include <Servo.h> 
-//#include "ServoTimer2.h" 
+ 
 
 #define LED_DEBUG 13
 #define PIN_TX 7
@@ -71,17 +71,13 @@ int servo_horz_pos = 1700;
 
 void setup()
 {
+    watchdog_setup();
+    
     Serial.begin(9600); 
     
     pinMode(LED_DEBUG, OUTPUT);     
     pinMode(PIN_TX, OUTPUT); 
-   
-    //vw_set_ptt_inverted(true); // Required for DR3100
-    //vw_setup(2000);      // Bits per sec
-    //vw_set_tx_pin(PIN_TX);
-      
-    watchdog_setup();
-  
+
     servo_virt.attach(SERVO_PIN_VIRT, SERVO_VIRT_MIN, SERVO_VIRT_MAX);
     servo_horz.attach(SERVO_PIN_HORZ, SERVO_HORZ_MIN, SERVO_HORZ_MAX);
 
@@ -121,13 +117,6 @@ void loop()
         //vw_send((uint8_t *)msg, strlen(msg));
         //vw_wait_tx(); // Wait until the whole message is gone
         
-        
-        if (tkr_dark_count > DARK_SLEEP_COUNT) {
-          tkr_dark_count = 0;  
-          //@todo goto sleep(), it's too dark.
-        }
-        
- 
     }
 }
 
@@ -187,12 +176,7 @@ int tkr_diff_virt()
 {
     int up = analogRead(PIN_LDR_UP);
     int down = analogRead(PIN_LDR_DOWN);
-    
-    // Count the dark readings.
-    if (up < THRESHOLD_DARK) {
-      //++tkr_dark_count;  
-    }
-    
+
     /*
     Serial.print(up); 
     Serial.print(":");
@@ -227,34 +211,13 @@ int tkr_diff_horz()
     return 0;
 }
 
-long readVcc() {
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
-  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
-  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    ADMUX = _BV(MUX3) | _BV(MUX2);
-  #else
-    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #endif  
- 
-  _delay_ms(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
- 
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
-  uint8_t high = ADCH; // unlocks both
- 
-  long result = (high<<8) | low;
- 
-  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-  return result; // Vcc in millivolts
-}
 
 void watchdog_setup()
 {
+  
+  // Clear any previous watchdog interupt
+  MCUSR = 0;
+  
   // Reset after 8 seconds, 
   // unless wdt_reset(); has been successfully called
   wdt_enable(WDTO_8S);
