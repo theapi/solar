79f78135926e627ec133b304385d91f74f413c07
theapi <petermclarke@gmail.com>
2014-05-13 21:17:09 +0100

    Removed the macros as this is not for debugging

--- i2c_master/i2c_master.ino
+++ i2c_master/i2c_master.ino
@@ -1,51 +1,49 @@
 #include <Wire.h>
 
-const byte SLAVE_ADDRESS = 100;  // which address debugging goes to
+const byte I2C_SLAVE_ADDRESS = 100;  // which address messages goes to
 
-
-  #define BEGIN_DEBUG   Wire.begin ()
+/*
   #define TRACE(x)      I2Cdebug.print   (x)
   #define TRACE2(x,y)   I2Cdebug.print   (x,y)
   #define TRACELN(x)    I2Cdebug.println (x)
   #define TRACELN2(x,y) I2Cdebug.println (x,y)
-  
-  class tI2Cdebug : public Print
-  {
+*/
+
+// Extend the print class for use with i2c
+class tI2C : public Print
+{
   public:
     virtual size_t write (const byte c)  { 
-      Wire.beginTransmission (SLAVE_ADDRESS);
+      Wire.beginTransmission (I2C_SLAVE_ADDRESS);
       Wire.write (c);
       Wire.endTransmission ();
-    }  // end of tI2Cdebug::write
-  }; // end of tI2Cdebug
+    }
+};
       
-  // an instance of the I2Cdebug object
-  tI2Cdebug I2Cdebug;
+// an instance of the I2C object
+tI2C I2C;
 
 
 long counter;
 unsigned long start;
 
-void setup() {
-  start = micros ();
-
-  BEGIN_DEBUG;
-  TRACELN ("Commenced device-under-test debugging!");
-}  // end of setup
+void setup() 
+{
+  start = micros();
+  Wire.begin();
+}  
 
 void loop() 
 {
-
   counter++;
-  if (counter == 100000)
-  {
-    TRACELN ("100000 reached.");
-    TRACE ("took ");
-    TRACE (micros () - start);
-    TRACE (" in HEX is: ");
-    TRACELN2 (micros () - start, HEX);
+  if (counter == 100000) {
+    I2C.println("100000 reached.");
+    I2C.print("took ");
+    I2C.print(micros() - start);
+    I2C.print(" in HEX is: ");
+    I2C.println(micros() - start, HEX);
   
     counter = 0;
-  }  // end of if
+  }
 
-}  // end of loop
+} 
