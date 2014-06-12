4a40329e22f1483c668c7df98f8847975f5e21d0
Peter <petermclarke@gmail.com>
2014-06-12 14:00:14 +0100

    Graphing

---
+++ datalog/www/csv.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Provide a csv to dygraph with the column headers added.
+ */
+ 
+$dir = "/mnt/storage/share/solar/datalog";
+
+$filenames = array();
+foreach (glob("$dir/*.csv") as $filename) {
+    $filenames[] = $filename;
+}
+rsort($filenames);
+$filename = current($filenames);
+
+echo "Time,MsgId,Temp1,Temp2,Soil,Solar,Battery\n";
+$handle = fopen($filename, "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        echo $buffer;
+    }
+    fclose($handle);
+}
