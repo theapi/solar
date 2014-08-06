c20455d24ea22465a9afc242c6caf329beed0e3a
theapi <petermclarke@gmail.com>
2014-08-06 18:41:56 +0100

    Initial message parsing

--- attiny85/receiver/receiver.ino
+++ attiny85/receiver/receiver.ino
@@ -8,14 +8,18 @@
 
 #define SERIAL_TX_PIN 3
 #define SERIAL_RX_PIN 4
+
+#define VW_MAX_MESSAGE_LEN 40 // Same as solar/sensor
 #define VW_RX_PIN 2
 
 SoftwareSerial mySerial(SERIAL_RX_PIN, SERIAL_TX_PIN); // RX, TX
 
+const byte debug_led = 0;
+
 void setup()
 {
 
-  pinMode(0, OUTPUT);    
+  pinMode(debug_led, OUTPUT);    
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
@@ -26,23 +30,63 @@ void setup()
   vw_rx_start();       // Start the receiver running
 }
 
+
 void loop()
 {
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    
-    digitalWrite(0, true);
-    
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-	int i;
-        for (i = 0; i < buflen; i++)
-        {
-            //Serial.print(char(buf[i]));
-            mySerial.write(char(buf[i]));
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  
+  digitalWrite(debug_led, HIGH);
+  
+  if (vw_get_message(buf, &buflen)) { // Non-blocking
+    int i;
+    int val = 0;
+    byte comma = 0;
+    for (i = 0; i < buflen; i++) {
+      if (buf[i] == 44) {      
+        // CSV - S,88,20.17,21.91,871,5595,5436
+        // comma 5 = solar panel reading
+        // comma 6 = battery reading
+        
+        if (comma == 5) {
+          // Print the solar reading
+          mySerial.print(" Solar ("); 
+          mySerial.print(val, DEC); 
+          mySerial.print(")"); 
         }
-        mySerial.write('\n'); 
+        
+        ++comma;
+        val = 0;
+      }
+      
+      mySerial.write(char(buf[i]));
+      
+      if (comma == 5 || comma == 6) {
+        byte c = buf[i];
+        // Only interested in numbers.
+        // ASCII hex values 0x30 to 0x30 are decimal 0 to 9
+        if (c > 0x2F && c < 0x3A) {
+            if (val > 0) {
+                // Expecting decimal numbers like 24,
+                // so for each new argument multiply by ten.
+                val *= 10;
+                val += c - 0x30;
+            } else {
+                val = c - 0x30;
+            }
+        }
+      }
+      
     }
+    
+    // Print the battery reading
+    mySerial.print(" Battery ("); 
+    mySerial.print(val, DEC); 
+    mySerial.print(")");
+    
+    mySerial.println(); 
+    
+  }
 
-    digitalWrite(0, false);
+  digitalWrite(debug_led, LOW);
 }
