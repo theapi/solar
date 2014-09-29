ae3745ab3b5eebec11cba6b4fc17fe01f04def47
theapi <petermclarke@gmail.com>
2014-09-29 21:19:43 +0100

    Use PWM

--- receiver_analog/receiver_analog.ino
+++ receiver_analog/receiver_analog.ino
@@ -9,15 +9,15 @@
 #include <VirtualWire.h>
 
 // inslude the SPI library:
-#include <SPI.h>
+//#include <SPI.h>
 
 #define DEBUG_LED_PIN 5 // PD5 (11) - Red led button
 #define DEBUG_LED_PWM_AWAKE 127 // pwm value to show contoller is awake
 #define DEBUG_LED_PWM_MSG 255 // pwm value to show message received
 
-//#define PWM_PIN 9 // TMP until dedicated chip arrives
-#define POT_CHIP_SELECT 10
-#define POT_ADDRESS 0
+#define PWM_PIN 11 // timer 2
+//#define POT_CHIP_SELECT 10
+//#define POT_ADDRESS 0
 
 #define VW_MAX_MESSAGE_LEN 40 // Same as solar/sensor
 #define VW_RX_PIN 7 // PD7 (13)
@@ -26,7 +26,7 @@
 #define WD_DO_STUFF 8 // How many watchdog interupts before doing real work (same a sensor).
 
 
-
+byte sleep_mode = SLEEP_MODE_PWR_SAVE;
 int parsed_val = 0;
 byte solar_val = 64; // Temporary half value
 volatile byte wd_isr = WD_DO_STUFF;
@@ -41,7 +41,7 @@ ISR(WDT_vect)
   } else {
       --wd_isr; 
       // Go back to sleep.
-      goToSleep();
+      goToSleep(sleep_mode);
   }
 }
 
@@ -53,6 +53,9 @@ void setup()
   // disable analog comparitor
   ACSR = (1 << ACD) | (0 << ACIE);
   
+  power_spi_disable();
+  power_twi_disable();
+  
   watchdog_setup();
   
   pinMode(DEBUG_LED_PIN, OUTPUT);  
@@ -62,12 +65,12 @@ void setup()
   digitalWrite(RF_POWER_PIN, HIGH);  
   
   // set the slaveSelectPin as an output:
-  pinMode (POT_CHIP_SELECT, OUTPUT);
+  //pinMode (POT_CHIP_SELECT, OUTPUT);
   // initialize SPI:
-  SPI.begin();
+  //SPI.begin();
   
-  //pinMode(PWM_PIN, OUTPUT);  // TMP until dedicated chip arrives
-  //analogWrite(PWM_PIN, solar_val);
+  pinMode(PWM_PIN, OUTPUT);  
+  analogWrite(PWM_PIN, 255);
 
   Serial.begin(9600);
   Serial.println("Setup");
@@ -76,7 +79,6 @@ void setup()
   vw_setup(2000);	 // Bits per sec
   vw_rx_start();       // Start the receiver running
   
-  digitalPotWrite(POT_ADDRESS, 128);
 }
 
 
@@ -105,7 +107,7 @@ void loop()
         
         if (comma == 5) {
           parsed_val = val;
-          solar_val = map(val, 0, 10000, 0, 128);
+          solar_val = map(val, 0, 10000, 0, 255);
           
           
           // Print the solar reading
@@ -150,15 +152,13 @@ void loop()
     
     
     Serial.println(); 
-    
-    // Ofcourse this is rather pointless a sleep will kill the pwm (replacing with digital pot)
-    //analogWrite(PWM_PIN, solar_val);
-    
+        
     Serial.print(parsed_val);
     Serial.print(" -> ");
     Serial.println(solar_val); 
     
-    digitalPotWrite(POT_ADDRESS, solar_val);
+    //digitalPotWrite(POT_ADDRESS, solar_val);
+    analogWrite(PWM_PIN, solar_val);
     
     // Turn off message indicator
     analogWrite(DEBUG_LED_PIN, DEBUG_LED_PWM_AWAKE);
@@ -167,11 +167,17 @@ void loop()
     Serial.flush();
     
     // Sleep 'till the next message is due
-    goToSleep(); 
+    if (solar_val > 0) {
+      goToSleep(SLEEP_MODE_PWR_SAVE); 
+    } else {
+      // no need for pwm so full sleep
+      goToSleep(SLEEP_MODE_PWR_DOWN); 
+    }
   }
 
 }
 
+/*
 void digitalPotWrite(int address, int value) {
   // take the SS pin low to select the chip:
   digitalWrite(POT_CHIP_SELECT, LOW);
@@ -181,6 +187,7 @@ void digitalPotWrite(int address, int value) {
   // take the SS pin high to de-select the chip:
   digitalWrite(POT_CHIP_SELECT, HIGH); 
 }
+*/
 
 /**
  * Watchdog for while awake to ensure things are ticking over.
@@ -207,9 +214,10 @@ void watchdog_setup()
   wdt_reset();
 }
 
-void goToSleep()
+void goToSleep(byte mode)
 {
-
+  sleep_mode = mode;
+  
   // Turn off the RF receiver
   digitalWrite(RF_POWER_PIN, LOW);
   
@@ -217,13 +225,22 @@ void goToSleep()
   digitalWrite(DEBUG_LED_PIN, LOW);
  
   cli();
-  
+    
   // ensure ADC is off
   ADCSRA = 0;
   
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  power_all_disable();
-
+  if (mode == SLEEP_MODE_PWR_DOWN) {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    power_all_disable();
+  } else {
+    // Keep pwm on
+    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+    power_timer0_disable();
+    power_timer1_disable();
+    power_usart0_disable();
+    // leaving timer2 on for the pwm
+  }
+  
   sleep_enable();
   sei();
   
@@ -235,7 +252,9 @@ void goToSleep()
   sleep_disable();  
   MCUSR = 0; // clear the reset register 
   
-  power_all_enable(); 
+  power_timer0_enable();
+  power_timer1_enable();
+  power_usart0_enable();
 
   // turn on the RF receiver
   digitalWrite(RF_POWER_PIN, HIGH);
