194a7e3e9ab93b8277820159cac2f409ab4e2fa4
Peter <petermclarke@gmail.com>
2014-09-30 13:31:33 +0100

    Set pwm frequency to prevent buzz on the analogue voltmeter

--- receiver_analog/receiver_analog.ino
+++ receiver_analog/receiver_analog.ino
@@ -58,6 +58,9 @@ void setup()
   
   watchdog_setup();
   
+  // Try a lower frequency pwm to prevent the buzzzzz from the analogue voltmeter.
+  setPwmFrequency(256);
+  
   pinMode(DEBUG_LED_PIN, OUTPUT);  
   analogWrite(DEBUG_LED_PIN, DEBUG_LED_PWM_AWAKE);
   
@@ -175,6 +178,38 @@ void loop()
     }
   }
 
+}
+
+/**
+ * The human hearing range is commonly given as 20 to 20,000 Hz
+ *
+ * @see http://playground.arduino.cc/Code/PwmFrequency
+ * @see http://playground.arduino.cc/Main/TimerPWMCheatsheet
+ * @see http://www.letsmakerobots.com/content/changing-pwm-frequencies-arduino-controllers
+ */
+void setPwmFrequency(int divisor) 
+{
+  // NB: Half these frequencies as using internal 8mhz clock.
+  //TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000100;    // set timer 2 divisor to    64 for PWM frequency of   490.20 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000101;    // set timer 2 divisor to   128 for PWM frequency of   245.10 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000110;    // set timer 2 divisor to   256 for PWM frequency of   122.55 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000111;    // set timer 2 divisor to  1024 for PWM frequency of    30.64 Hz
+
+  byte mode;
+  switch(divisor) {
+    case 1: mode = 0x01; break;
+    case 8: mode = 0x02; break;
+    case 32: mode = 0x03; break;
+    case 64: mode = 0x04; break;
+    case 128: mode = 0x05; break;
+    case 256: mode = 0x06; break;
+    case 1024: mode = 0x7; break;
+    default: return;
+  }
+  TCCR2B = TCCR2B & 0b11111000 | mode;
 }
 
 /*
