e601edab6a28b6468c7434b65e2645ca30abdd89
theapi <petermclarke@gmail.com>
2014-10-01 20:59:49 +0100

    Stand alone

--- receiver_datalog/receiver_datalog.ino
+++ receiver_datalog/receiver_datalog.ino
@@ -1,48 +1,62 @@
+
 /**
  * Listen to the RF transmitter and print it to the serial monitor.
  */
+ 
+#include <avr/wdt.h>
 #include <VirtualWire.h>
 
+#define DEBUG_PIN 13
 
 // Pin 11 is the default receiver pin.
 // @see http://www.airspayce.com/mikem/arduino/VirtualWire_8h.html#ae62b601260ae59e7e83c1e63ae0c064b
+#define VW_RX_PIN 7
+#define VW_MAX_MESSAGE_LEN 40 // Same as solar/sensor
 
-int received_reset_interval = 60 * 60 * 1000; // How often to reset the received counter.
-int received_count; // How many transmissions have been received.
-unsigned long received_last_reset;
+int heartbeat_interval = 6 * 1000; // How often to send the heartbeat.
+unsigned long heartbeat_last;
 
 void setup() 
 {
-  pinMode(13, OUTPUT);
+  // Clear any previous watchdog interupt
+  MCUSR = 0;
+  wdt_enable(WDTO_8S);
+  
+  pinMode(DEBUG_PIN, OUTPUT);
   
   // initialize serial communication
   Serial.begin(9600);
   Serial.println("setup");
+
+
   
-  received_count = 0;
-  received_last_reset = millis();
-  
-  // Initialise the IO and ISR
-  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_set_rx_pin(VW_RX_PIN);
   vw_setup(2000);      // Bits per sec
-  // vw_set_tx_pin(11);
   vw_rx_start();       // Start the receiver PLL running
+  
+
+  Serial.println("lets go..");
 }
 
 void loop()
 {
+  
+  // Pat the dog
+  wdt_reset();
+  
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   
   unsigned long now = millis();
-  if (now - received_last_reset > received_reset_interval) {
-    received_last_reset = now;  
-    received_count = 0;
+  if (now - heartbeat_last > heartbeat_interval) {
+    heartbeat_last = now;
+    Serial.print("HeartBeat: ");
+    Serial.println(now);
   }
   
   if (vw_get_message(buf, &buflen)) {
       int i;
-      digitalWrite(13, HIGH); // Flash a light to show received good message
+      digitalWrite(DEBUG_PIN, HIGH); // Flash a light to show received good message
       // Message with a good checksum received, output it to serial.
       Serial.print("R: ");
       for (i = 0; i < buflen; i++) {
@@ -50,11 +64,7 @@ void loop()
       }
       Serial.println("");
       
-      ++received_count;
-      Serial.print("C: ");
-      Serial.println(received_count);
-      
-      digitalWrite(13, LOW);
+      digitalWrite(DEBUG_PIN, LOW);
   }
 
 }
