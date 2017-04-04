31f2bed0e00fb06c886783cbda6a990832d1851c
Peter Clarke <petermclarke@gmail.com>
2017-04-04 13:59:02 +0100

    Initial LoRa test

---
+++ experiments/lora_test/lora_test.ino
@@ -0,0 +1,249 @@
+/**
+ *  ping-pong test by requesting an ACK from the gateway
+ *  
+ *  @link https://github.com/theapi/LowCostLoRaGw/blob/master/Arduino/Arduino_LoRa_Ping_Pong/Arduino_LoRa_Ping_Pong.ino
+ *  
+ */
+#include <SPI.h>  
+// Include the SX1272
+#include "SX1272.h"
+// oled display
+#include "U8glib.h"
+
+
+// IMPORTANT
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// please uncomment only 1 choice
+//
+#define ETSI_EUROPE_REGULATION
+//#define FCC_US_REGULATION
+//#define SENEGAL_REGULATION
+/////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+// IMPORTANT
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// uncomment if your radio is an HopeRF RFM92W, HopeRF RFM95W, Modtronix inAir9B, NiceRF1276
+// or you known from the circuit diagram that output use the PABOOST line instead of the RFO line
+#define PABOOST
+/////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+// IMPORTANT
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// please uncomment only 1 choice
+#define BAND868
+//#define BAND900
+//#define BAND433
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef ETSI_EUROPE_REGULATION
+#define MAX_DBM 14
+#elif defined SENEGAL_REGULATION
+#define MAX_DBM 10
+#elif defined FCC_US_REGULATION
+#define MAX_DBM 14
+#endif
+
+#ifdef BAND868
+#ifdef SENEGAL_REGULATION
+const uint32_t DEFAULT_CHANNEL=CH_04_868;
+#else
+const uint32_t DEFAULT_CHANNEL=CH_10_868;
+#endif
+#elif defined BAND900
+const uint32_t DEFAULT_CHANNEL=CH_05_900;
+#elif defined BAND433
+const uint32_t DEFAULT_CHANNEL=CH_00_433;
+#endif
+
+///////////////////////////////////////////////////////////////////
+// CHANGE HERE THE LORA MODE, NODE ADDRESS 
+#define LORAMODE  1
+#define node_addr 8
+//////////////////////////////////////////////////////////////////
+
+// we wrapped Serial.println to support the Arduino Zero or M0
+#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0
+#define PRINTLN                   SerialUSB.println("")              
+#define PRINT_CSTSTR(fmt,param)   SerialUSB.print(F(param))
+#define PRINT_STR(fmt,param)      SerialUSB.print(param)
+#define PRINT_VALUE(fmt,param)    SerialUSB.print(param)
+#define FLUSHOUTPUT               SerialUSB.flush();
+#else
+#define PRINTLN                   Serial.println("")
+#define PRINT_CSTSTR(fmt,param)   Serial.print(F(param))
+#define PRINT_STR(fmt,param)      Serial.print(param)
+#define PRINT_VALUE(fmt,param)    Serial.print(param)
+#define FLUSHOUTPUT               Serial.flush();
+#endif
+
+#define DEFAULT_DEST_ADDR 1
+
+uint8_t message[100];
+
+int loraMode=LORAMODE;
+
+
+// OLED
+U8GLIB_SSD1306_128X64_2X u8g(U8G_I2C_OPT_NONE);
+
+typedef struct{
+  int num;
+  int code;
+
+}
+monitor_t;
+monitor_t monitor;
+
+int num = 0;
+
+void setup()
+{
+  int e;
+
+  memset(&monitor, 0, sizeof(monitor_t));
+  
+  // Open serial communications and wait for port to open:
+#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0 
+  SerialUSB.begin(38400);
+#else
+  Serial.begin(38400);  
+#endif 
+
+  // Print a start message
+  PRINT_CSTSTR("%s","Simple LoRa ping-pong with the gateway\n");  
+
+#ifdef ARDUINO_AVR_PRO
+  PRINT_CSTSTR("%s","Arduino Pro Mini detected\n");
+#endif
+
+#ifdef ARDUINO_AVR_NANO
+  PRINT_CSTSTR("%s","Arduino Nano detected\n");
+#endif
+
+#ifdef ARDUINO_AVR_MINI
+  PRINT_CSTSTR("%s","Arduino MINI/Nexus detected\n");
+#endif
+
+#ifdef __MK20DX256__
+  PRINT_CSTSTR("%s","Teensy31/32 detected\n");
+#endif
+
+#ifdef __SAMD21G18A__ 
+  PRINT_CSTSTR("%s","Arduino M0/Zero detected\n");
+#endif
+
+  // Power ON the module
+  sx1272.ON();
+  
+  // Set transmission mode and print the result
+  e = sx1272.setMode(loraMode);
+  PRINT_CSTSTR("%s","Setting Mode: state ");
+  PRINT_VALUE("%d", e);
+  PRINTLN;
+
+  // enable carrier sense
+  sx1272._enableCarrierSense=true;
+    
+  // Select frequency channel
+  e = sx1272.setChannel(DEFAULT_CHANNEL);
+  PRINT_CSTSTR("%s","Setting Channel: state ");
+  PRINT_VALUE("%d", e);
+  PRINTLN;
+  
+  // Select amplifier line; PABOOST or RFO
+#ifdef PABOOST
+  sx1272._needPABOOST=true;
+  // previous way for setting output power
+  // powerLevel='x';
+#else
+  // previous way for setting output power
+  // powerLevel='M';  
+#endif
+
+  // previous way for setting output power
+  // e = sx1272.setPower(powerLevel); 
+
+  e = sx1272.setPowerDBM((uint8_t)MAX_DBM); 
+  PRINT_CSTSTR("%s","Setting Power: state ");
+  PRINT_VALUE("%d", e);
+  PRINTLN;
+  
+  // Set the node address and print the result
+  e = sx1272.setNodeAddress(node_addr);
+  PRINT_CSTSTR("%s","Setting node addr: state ");
+  PRINT_VALUE("%d", e);
+  PRINTLN;
+  
+  // Print a success message
+  PRINT_CSTSTR("%s","SX1272 successfully configured\n");
+
+  delay(500);
+}
+
+
+void loop(void)
+{
+  uint8_t r_size;
+  int e;
+
+  sx1272.CarrierSense();
+
+  sx1272.setPacketType(PKT_TYPE_DATA);
+
+  r_size=sprintf((char*)message, "Ping");
+      
+  while (1) {
+
+      PRINT_CSTSTR("%s","Sending Ping");  
+      PRINTLN;
+            
+      e = sx1272.sendPacketTimeoutACK(DEFAULT_DEST_ADDR, message, r_size);
+
+      // this is the no-ack version
+      // e = sx1272.sendPacketTimeout(DEFAULT_DEST_ADDR, message, r_size);
+            
+      PRINT_CSTSTR("%s","Packet sent, state ");
+      PRINT_VALUE("%d", e);
+      PRINTLN;
+      
+      if (e==3)
+          PRINT_CSTSTR("%s","No Pong!");
+        
+      if (e==0)
+          PRINT_CSTSTR("%s","Pong received from gateway!");      
+
+      PRINTLN;
+
+      monitor.num = ++num;
+      monitor.code = e;
+      displayUpdate();
+      
+      delay(10000);    
+  }          
+}
+
+void displayUpdate()
+{
+  // picture loop
+  u8g.firstPage();
+  do {
+    draw();
+  } while( u8g.nextPage() );
+}
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here
+
+  u8g.setFont(u8g_font_fub11n);
+  //u8g.setFont(u8g_font_unifont);
+  u8g.setFontPosTop();
+  
+  u8g.setPrintPos(0, 0);
+  u8g.print(monitor.num);
+  
+  u8g.setPrintPos(90, 0);
+  u8g.print(monitor.code);
+  
+}
+
