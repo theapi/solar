b8b31a4e6f1666dcab87c97c3bd72e9d62b56007
theapi <petermclarke@gmail.com>
2017-04-16 21:02:03 +0100

    Broadcast LoRa data via UDP

--- experiments/esp8266_monitor/esp8266_monitor.ino
+++ experiments/esp8266_monitor/esp8266_monitor.ino
@@ -1,21 +1,24 @@
 
-#include "config.h"
-#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
-#include "Payload.h"
+
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "config.h"
+#include "SSD1306.h"
+#include "Payload.h"
 
-//how many clients should be able to telnet to this ESP8266
-#define MAX_SRV_CLIENTS 2
 
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
 
-WiFiServer server(23);
-WiFiClient serverClients[MAX_SRV_CLIENTS];
+// Multicast declarations
+IPAddress ipMulti(239, 0, 0, 57);
+unsigned int portMulti = 12345;      // local port to listen on
+
 
 // Initialize the OLED display using Wire library
 SSD1306  display(0x3c, 4, 5);
 
 Payload rx_payload = Payload();
-Payload ping_payload = Payload();
 uint8_t input_string[Payload_SIZE];
 uint8_t payload_state = 0;
 uint8_t serial_byte_count = 0;
@@ -25,7 +28,7 @@ const long interval = 100;
 
 const long ping_interval = 3000;
 unsigned long ping_last = 0;
-uint8_t ping_msg_id = 0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -35,8 +38,6 @@ void setup() {
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_16);
-
-  ping_payload.setDeviceId(254);
 
   rx_payload.setMsgId(254);
   rx_payload.setA(1234);
@@ -58,23 +59,17 @@ void setup() {
     while(1) delay(500);
   }
 
-  // Socket server.
-  server.begin();
-  server.setNoDelay(true);
-  
-  Serial.print("Ready! Use 'netcat ");
-  Serial.print(WiFi.localIP());
-  Serial.println(" 23' to connect");
+  Serial.print("Connected to: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Ready! Listen for UDP broadcasts on 239.0.0.57 port 12345");
 }
 
 void loop() {
-
 
   // Read the data from the LoRa receiver.
   while (Serial.available()) {
     // get the new byte:
     uint8_t in = (uint8_t) Serial.read();
-    
     
     if (payload_state == 0) {
       // Check for the start of the payload
@@ -96,55 +91,11 @@ void loop() {
         serial_byte_count = 0;
         payload_state = 2;
         rx_payload.unserialize(input_string);
-//        Serial.println();
-//        Serial.print(rx_payload.getDeviceId()); Serial.print(", ");
-//        Serial.print(rx_payload.getMsgId()); Serial.print(", ");
-//        Serial.print(rx_payload.getA()); Serial.print(", ");
-//        Serial.print(rx_payload.getB()); Serial.print(", ");
-//        Serial.print(rx_payload.getC()); Serial.print(", ");
-//        Serial.print(rx_payload.getD()); Serial.print(", ");
-//        Serial.print(rx_payload.getE()); Serial.print(", ");
-//        Serial.println(rx_payload.getF());
-        
       }
     } else {
       // Passthru other serial messages.
       //Serial.print(char(in));
       
-    }
-  }
-
-  uint8_t i;
-  // check if there are any new TCP clients
-  if (server.hasClient()) {
-    uint8_t allocated = 0;
-    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-      if (allocated == 0) {
-        //find free/disconnected spot
-        if (!serverClients[i] || !serverClients[i].connected()) {
-          if (serverClients[i]) {
-            serverClients[i].stop();
-          }
-          serverClients[i] = server.available();
-          serverClients[i].setNoDelay(true);
-          serverClients[i].setTimeout(100);
-          Serial.print("New client: "); Serial.println(i);
-          allocated = 1;
-        }
-      }
-    }
-    //no free/disconnected spot so reject
-    WiFiClient serverClient = server.available();
-    serverClient.stop();
-  }
-  
-  // check clients for data
-  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-    if (serverClients[i] && serverClients[i].connected()) {
-      if (serverClients[i].available()) {
-        // Ignore client input
-        serverClients[i].flush();
-      }
     }
   }
 
@@ -156,51 +107,13 @@ void loop() {
 
     // No need to ping if we're sending real data.
     ping_last = currentMillis;
-    
-    size_t len = Payload_SIZE;
-    uint8_t sbuf[len];
-    rx_payload.serialize(sbuf);
-
-    // push payload data to all connected clients
-    for(i = 0; i < MAX_SRV_CLIENTS; i++){
-      if (serverClients[i] && serverClients[i].connected()) {
-        if (!serverClients[i].write('\t')) { // Payload start byte
-          serverClients[i].stop();
-        }
-        else {
-          serverClients[i].write(sbuf, len);
-          serverClients[i].write('\n');
-        }
-      }
-    }
+    broadcast_udp();
   } 
-  // Keep TCP clients connected with a ping.
+  // Send the data continually, as its UDP some may get missed.
   else if (currentMillis - ping_last >= ping_interval) {
     ping_last = currentMillis;
-
-    ping_payload.setMsgId(++ping_msg_id);
-    //ping_payload.setF(1234);
-    // Send a ping message.
-    size_t len = Payload_SIZE;
-    uint8_t sbuf[len];
-    ping_payload.serialize(sbuf);
-
-    // push payload data to all connected clients
-    for(i = 0; i < MAX_SRV_CLIENTS; i++){
-      if (serverClients[i] && serverClients[i].connected()) {
-        if (!serverClients[i].write('\t')) { // Payload start byte
-          serverClients[i].stop();
-          Serial.print("Stopped: ");
-          Serial.println(i);
-        }
-        else {
-          serverClients[i].write(sbuf, len);
-          serverClients[i].write('\n');
-        }
-      }
-    }
+    broadcast_udp();
   }
-
 
   // Update the display  
   if (currentMillis - previousMillis >= interval) {
@@ -218,5 +131,17 @@ void loop() {
     display.display();
   }
   
+}
+
+void broadcast_udp() {
+    size_t len = Payload_SIZE;
+    uint8_t sbuf[len];
+    rx_payload.serialize(sbuf);
+    Udp.beginPacketMulticast(ipMulti, portMulti, WiFi.localIP());
+    Udp.write('\t'); // Payload start byte
+    Udp.write(sbuf, len);
+    Udp.write('\n');
+    Udp.endPacket();  
+    Udp.stop();  
 }
 
