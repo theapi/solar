9ef48c550c0e8f8ed14ab941d36afbf5abd2d355
Peter Clarke <petermclarke@gmail.com>
2017-05-05 11:04:39 +0100

    Removed OLED, and handle slow switching to Internal aref

--- LoRa/LoRa_Garden_TX/LoRa_Garden_TX.ino
+++ LoRa/LoRa_Garden_TX/LoRa_Garden_TX.ino
@@ -4,8 +4,7 @@
 #include <Crypto.h>
 #include <AES.h>
 #include <RH_RF95.h>
-// oled display
-#include "U8glib.h"
+
 #include "config.h"
 #include "GardenPayload.h"
 #include "AckPayload.h"
@@ -26,29 +25,15 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-// OLED
-U8GLIB_SSD1306_128X64_2X u8g(U8G_I2C_OPT_NONE);
-
 theapi::GardenPayload tx_payload = theapi::GardenPayload();
 theapi::AckPayload ack_payload = theapi::AckPayload();
 
 uint8_t msg_id = 0;
 int rssi = 0;
 
-typedef struct {
-  int sent;
-  int got;
-  int rssi;
-}
-monitor_t;
-monitor_t monitor;
-
 AES128 cipher;
 uint8_t encrypted_buffer[ENCRYPTION_BUFFER_SIZE];
 uint8_t decrypted_buf[ENCRYPTION_BUFFER_SIZE];
-
-uint8_t rx_node_id = 1;
-uint8_t garden_node_id = 2;
 
 void setup() {
   pinMode(RFM95_RST, OUTPUT);
@@ -56,11 +41,6 @@ void setup() {
 
   Serial.begin(115200);
 
-  //u8g.setRot180();
-  monitor.sent = 0;
-  monitor.got = 0;
-  monitor.rssi = 0;
-  displayUpdate();
   
   delay(100);
 
@@ -82,45 +62,42 @@ void setup() {
     Serial.println("setFrequency failed");
     while (1);
   }
-  //Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
-  
   rf95.setTxPower(10, false);
 }
-
 
 void loop() {
 
   tx_payload.setMsgId(++msg_id);
-  // Internal 1.1v aref
-  uint16_t vcc = readVcc();
-  tx_payload.setVcc(vcc);
+
+  // Must read the internal referenced analogs first.
   tx_payload.setChargeMa(readSolarCurrent());
 
+  uint16_t vcc = readVcc();
+  tx_payload.setVcc(vcc);
+
+  // Switch the reference back to vcc.
+  analogReference(DEFAULT);
+  analogRead(PIN_SENSOR_SOLAR_VOLTS);
+
+  // Read the temperature while the aref settles.
+  tx_payload.setTemperature(readTemperature());
+  
   // Vcc aref
   tx_payload.setChargeMv(readSolarVolts(vcc));
   tx_payload.setLight(readLight(vcc));
   tx_payload.setSoil(readSoil(vcc));
   
-  tx_payload.setTemperature(readTemperature());
-  
+  // Build the payload buffer.
   uint8_t payload_buf[tx_payload.size()];
   tx_payload.serialize(payload_buf);
-  //rf95.send(payload_buf, tx_payload.size());
+  // Encrypt the payload.
   cipher.encryptBlock(encrypted_buffer, payload_buf);
-  //rf95.setHeaderTo(rx_node_id);
-  //rf95.setHeaderFrom(garden_node_id);
+  // Send it.
   rf95.send(encrypted_buffer, ENCRYPTION_BUFFER_SIZE);
-
-  
-  int got = 0;
-  monitor.sent = msg_id;
-  monitor.got = got;
-  monitor.rssi = rssi;
-  displayUpdate();
-
   rf95.waitPacketSent();
+  
   // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
@@ -129,27 +106,14 @@ void loop() {
     // Should be a reply message for us now   
     if (rf95.recv(buf, &len)) {
       RH_RF95::printBuffer("Received: ", buf, len);
-      //Serial.print("headerFrom: "); Serial.println(rf95.headerFrom());
-      //Serial.print("Got reply: ");
-      //Serial.println((char*)buf);
-      //got = atoi((char*)buf);
-      //Serial.println(got);
       Serial.print("RSSI: ");
       rssi = rf95.lastRssi();
       Serial.println(rssi, DEC);    
 
-      //uint8_t ack_payload_buf[ack_payload.size()];
       uint8_t decrypted_ack_buf[16];
       cipher.decryptBlock(decrypted_ack_buf, buf);
       ack_payload.unserialize(decrypted_ack_buf);
 
-      got = ack_payload.getMsgId();
-      Serial.print("msg_id: ");
-      Serial.println(got, DEC);
-
-      monitor.got = got;
-      monitor.rssi = rssi;
-      displayUpdate();
     } else {
       Serial.println("Receive failed");
     }
@@ -157,35 +121,12 @@ void loop() {
     Serial.println("No reply");
   }
 
+  // Change to internal aref now so it has plenty of time to switch.
+  analogReference(INTERNAL);
+  // Need to make a reading for the ref change to take effect.
+  analogRead(PIN_SENSOR_SOLAR_CURRENT);
   
   delay(5000);
-}
-
-void displayUpdate()
-{
-  // picture loop
-  u8g.firstPage();
-  do {
-    draw();
-  } while( u8g.nextPage() );
-}
-
-void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here
-
-  u8g.setFont(u8g_font_fub11n);
-  //u8g.setFont(u8g_font_unifont);
-  u8g.setFontPosTop();
-  
-  u8g.setPrintPos(0, 0);
-  u8g.print(monitor.sent);
-
-  u8g.setPrintPos(60, 0);
-  u8g.print(monitor.got);
-  
-  u8g.setPrintPos(0, 20);
-  u8g.print(monitor.rssi);
-  
 }
 
 /**
@@ -220,13 +161,12 @@ uint16_t readVcc()
 
 uint16_t readSolarCurrent() {
   // 1 ohm inline resistor
-  analogReference(INTERNAL);
+  //analogReference(INTERNAL);
   uint16_t val;
-  // After changing the analog reference, 
-  // the first few readings from analogRead() may not be accurate.
-  for (uint8_t i = 0; i < 3; i++) { 
-    val = analogRead(PIN_SENSOR_SOLAR_CURRENT);
-  }
+
+  // This first reading after a mux change is unreliable.
+  val = analogRead(PIN_SENSOR_SOLAR_CURRENT);
+  
   // Average the next readings.
   val = 0;
   for (uint8_t i = 0; i < 2; i++) { 
@@ -238,13 +178,11 @@ uint16_t readSolarCurrent() {
 
 uint16_t readSolarVolts(uint16_t vcc) {
   // Voltage divider to half the incoming volts.
-  analogReference(DEFAULT);
   uint16_t val;
-  // After changing the analog reference, 
-  // the first few readings from analogRead() may not be accurate.
-  for (uint8_t i = 0; i < 2; i++) { 
-    val = analogRead(PIN_SENSOR_SOLAR_VOLTS);
-  }
+
+  // This first reading after a mux change is unreliable.
+  val = analogRead(PIN_SENSOR_SOLAR_VOLTS);
+  
   // Add the next 2 readings.
   // To avergage and account for the halving votage divider.
   val = 0;
@@ -256,13 +194,11 @@ uint16_t readSolarVolts(uint16_t vcc) {
 }
 
 uint16_t readLight(uint16_t vcc) {
-  analogReference(DEFAULT);
   uint16_t val;
-  // After changing the analog reference, 
-  // the first few readings from analogRead() may not be accurate.
-  for (uint8_t i = 0; i < 2; i++) { 
-    val = analogRead(PIN_SENSOR_LIGHT);
-  }
+
+  // This first reading after a mux change is unreliable.
+  val = analogRead(PIN_SENSOR_LIGHT);
+  
   // Average the next 2 readings.
   val = 0;
   for (uint8_t i = 0; i < 2; i++) { 
@@ -273,13 +209,11 @@ uint16_t readLight(uint16_t vcc) {
 }
 
 uint16_t readSoil(uint16_t vcc) {
-  analogReference(DEFAULT);
   uint16_t val;
-  // After changing the analog reference, 
-  // the first few readings from analogRead() may not be accurate.
-  for (uint8_t i = 0; i < 2; i++) { 
-    val = analogRead(PIN_SENSOR_SOIL);
-  }
+
+  // This first reading after a mux change is unreliable.
+  val = analogRead(PIN_SENSOR_SOIL);
+
   // Average the next 2 readings.
   val = 0;
   for (uint8_t i = 0; i < 2; i++) { 
