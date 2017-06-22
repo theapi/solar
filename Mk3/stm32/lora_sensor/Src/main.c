fbc0e7ecd5511b5c4da8cc07d251ae1892d50cdb
Peter Clarke <petermclarke@gmail.com>
2017-06-22 12:08:27 +0100

    Formatting

--- Mk3/stm32/lora_sensor/Src/main.c
+++ Mk3/stm32/lora_sensor/Src/main.c
@@ -1,40 +1,4 @@
-/**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2017 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32l0xx_hal.h"
@@ -67,201 +31,195 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 
-//uint8_t state;
-volatile uint8_t dio0_action = 0;
-MAIN_StateTypeDef state;
-
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
 
+volatile uint8_t dio0_action = 0;
+MAIN_StateTypeDef state;
+
 /* USER CODE END 0 */
 
-int main(void)
-{
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+    /* MCU Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_ADC_Init();
-  MX_I2C1_Init();
-  MX_RTC_Init();
-  MX_USART1_UART_Init();
-  MX_SPI2_Init();
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_ADC_Init();
+    MX_I2C1_Init();
+    MX_RTC_Init();
+    MX_USART1_UART_Init();
+    MX_SPI2_Init();
 
-  /* USER CODE BEGIN 2 */
+    /* USER CODE BEGIN 2 */
 
-  /* Enable Ultra low power mode */
+    /* Enable Ultra low power mode */
     HAL_PWREx_EnableUltraLowPower();
     __HAL_RCC_WAKEUPSTOP_CLK_CONFIG(RCC_STOP_WAKEUPCLOCK_HSI);
 
-  /* Buffer used for transmission on USART1 */
-char tx1_buffer[80];
+    /* Buffer used for transmission on USART1 */
+    char tx1_buffer[80];
 
-uint8_t count = 0;
+    uint8_t count = 0;
 
-uint8_t reg_val = 0;
+    uint8_t reg_val = 0;
 
-  /* SPI chip select high */
-  HAL_GPIO_WritePin(GPIOB, SPI2_CS_Pin, GPIO_PIN_SET);
-  HAL_Delay(30);
-  RFM95_init(&hspi2);
+    /* SPI chip select high */
+    HAL_GPIO_WritePin(GPIOB, SPI2_CS_Pin, GPIO_PIN_SET);
+    HAL_Delay(30);
+    RFM95_init(&hspi2);
 
+    uint8_t payload_buff[14];
+    PAYLOAD_Garden payload_garden;
+    payload_garden.MessageType = 50;
+    payload_garden.MessageId = 0;
+    payload_garden.VCC = 128;
+    payload_garden.ChargeMa = 124;
 
-  uint8_t payload_buff[14];
-  PAYLOAD_Garden payload_garden;
-  payload_garden.MessageType = 50;
-  payload_garden.MessageId = 0;
-  payload_garden.VCC = 128;
-  payload_garden.ChargeMa = 124;
+    // Start in sensing mode.
+    state = MAIN_STATE_SENSE;
 
-  // Start in sensing mode.
-  state = MAIN_STATE_SENSE;
+    /* USER CODE END 2 */
 
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        /* USER CODE END WHILE */
 
+        /* USER CODE BEGIN 3 */
 
-  /* USER CODE END 2 */
+        /* Do some work */
+        if (state == MAIN_STATE_SENSE) {
+            reg_val = RFM95_readRegister(&hspi2, RFM95_REG_OP_MODE);
+            sprintf(tx1_buffer, "Reg val: %02X\n", reg_val);
+            HAL_UART_Transmit(&huart1, (uint8_t*) tx1_buffer,
+                    strlen(tx1_buffer), 5000);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-  /* USER CODE END WHILE */
+            HAL_GPIO_WritePin(GPIOA, LED_Pin, GPIO_PIN_SET);
+            sprintf(tx1_buffer, "Count is %d\n", count);
+            HAL_UART_Transmit(&huart1, (uint8_t*) tx1_buffer,
+                    strlen(tx1_buffer), 5000);
+            count++;
 
-  /* USER CODE BEGIN 3 */
+            payload_garden.MessageId = count;
+            PAYLOAD_Garden_serialize(payload_garden, payload_buff);
 
-      /* Do some work */
-      if (state == MAIN_STATE_SENSE) {
-          reg_val = RFM95_readRegister(&hspi2, RFM95_REG_OP_MODE);
-          sprintf(tx1_buffer, "Reg val: %02X\n", reg_val);
-          HAL_UART_Transmit(&huart1, (uint8_t*)tx1_buffer,  strlen(tx1_buffer), 5000);
+            RFM95_send(&hspi2, payload_buff, 14);
 
-          HAL_GPIO_WritePin(GPIOA, LED_Pin, GPIO_PIN_SET);
-          sprintf(tx1_buffer, "Count is %d\n", count);
-          HAL_UART_Transmit(&huart1, (uint8_t*)tx1_buffer,  strlen(tx1_buffer), 5000);
-          count++;
+            state = MAIN_STATE_TX;
+        }
 
-          payload_garden.MessageId = count;
-          PAYLOAD_Garden_serialize(payload_garden, payload_buff);
+        /* Do nothing while the transmission is in progress */
+        else if (state == MAIN_STATE_TX && dio0_action == 1) {
+            RFM95_setMode(&hspi2, RFM95_MODE_SLEEP);
 
-          RFM95_send(&hspi2, payload_buff, 14);
+            state = MAIN_STATE_SLEEP;
+        }
 
-          state = MAIN_STATE_TX;
-      }
+        /* Now that all the work is done, sleep until its time to do it all again */
+        else if (state == MAIN_STATE_SLEEP) {
+            //HAL_Delay(1000);
 
-      /* Do nothing while the transmission is in progress */
-      else if (state == MAIN_STATE_TX && dio0_action == 1) {
-          RFM95_setMode(&hspi2, RFM95_MODE_SLEEP);
+            HAL_GPIO_WritePin(GPIOA, LED_Pin, GPIO_PIN_RESET);
 
-          state = MAIN_STATE_SLEEP;
-      }
+            /* Turn off the pin interrupts */
+            HAL_NVIC_DisableIRQ(EXTI4_15_IRQn);
 
-      /* Now that all the work is done, sleep until its time to do it all again */
-      else if (state == MAIN_STATE_SLEEP) {
-        //HAL_Delay(1000);
+            HAL_SuspendTick();
 
-        HAL_GPIO_WritePin(GPIOA, LED_Pin, GPIO_PIN_RESET);
+            /* Enter Stop Mode */
+            HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 2,
+                    RTC_WAKEUPCLOCK_CK_SPRE_16BITS);
+            HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
+            HAL_RTCEx_DeactivateWakeUpTimer(&hrtc);
+            HAL_ResumeTick();
 
-        /* Turn off the pin interrupts */
-        HAL_NVIC_DisableIRQ(EXTI4_15_IRQn);
+            /* Turn on the pin interrupts */
+            HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
 
-        HAL_SuspendTick();
+            state = MAIN_STATE_SENSE;
+        }
 
-        /* Enter Stop Mode */
-        HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 2, RTC_WAKEUPCLOCK_CK_SPRE_16BITS);
-        HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
-        HAL_RTCEx_DeactivateWakeUpTimer(&hrtc);
-        HAL_ResumeTick();
-
-        /* Turn on the pin interrupts */
-        HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
-
-        state = MAIN_STATE_SENSE;
-      }
-
-  }
-  /* USER CODE END 3 */
+    }
+    /* USER CODE END 3 */
 
 }
 
 /** System Clock Configuration
-*/
-void SystemClock_Config(void)
-{
+ */
+void SystemClock_Config(void) {
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
     /**Configure the main internal regulator output voltage 
-    */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+     */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
     /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI
+            | RCC_OSCILLATORTYPE_LSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 16;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
 
     /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+            | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_I2C1
-                              |RCC_PERIPHCLK_RTC;
-  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
-  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1
+            | RCC_PERIPHCLK_I2C1 | RCC_PERIPHCLK_RTC;
+    PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+    PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
 
     /**Configure the Systick interrupt time 
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+     */
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
     /**Configure the Systick 
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+     */
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* USER CODE BEGIN 4 */
@@ -287,51 +245,49 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
-void _Error_Handler(char * file, int line)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
+void _Error_Handler(char * file, int line) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
     char buffer[80];
     sprintf(buffer, "Error: %d\n", line);
-    HAL_UART_Transmit(&huart1, (uint8_t*)buffer,  strlen(buffer), 5000);
-  while(1) 
-  {
-      HAL_GPIO_TogglePin(GPIOA, LED_Pin);
-      HAL_Delay(100);
-  }
-  /* USER CODE END Error_Handler_Debug */ 
+    HAL_UART_Transmit(&huart1, (uint8_t*) buffer, strlen(buffer), 5000);
+    while (1) {
+        HAL_GPIO_TogglePin(GPIOA, LED_Pin);
+        HAL_Delay(100);
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
 
 /**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
+ * @brief Reports the name of the source file and the source line number
+ * where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t* file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
 
 }
 
 #endif
 
 /**
-  * @}
-  */ 
+ * @}
+ */
 
 /**
-  * @}
-*/ 
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
