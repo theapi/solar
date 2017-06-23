2884bfbbad4110c5aca9bb1d2842ca7dbfc5c8d4
Peter Clarke <petermclarke@gmail.com>
2017-06-23 17:33:33 +0100

    Ported functions

--- Mk3/stm32/lora_sensor/Src/ads1015.c
+++ Mk3/stm32/lora_sensor/Src/ads1015.c
@@ -1,19 +1,16 @@
 #include "ads1015.h"
 
-HAL_StatusTypeDef ADS1015_writeRegister(I2C_HandleTypeDef* hi2c,
-        uint8_t i2cAddress, uint8_t addr, uint16_t val) {
+HAL_StatusTypeDef ADS1015_writeRegister(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress, uint8_t addr, uint16_t val) {
     //@todo implement ADS1015_writeRegister
     return HAL_OK;
 }
 
-uint16_t ADS1015_readRegister(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress,
-        uint8_t addr) {
+uint16_t ADS1015_readRegister(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress, uint8_t addr) {
     //@todo implement ADS1015_readRegister
     return 0;
 }
 
-uint16_t ADS1015_SingleEnded(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress,
-        uint8_t channel, Gain_TypeDef gain) {
+uint16_t ADS1015_SingleEnded(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress, uint8_t channel, Gain_TypeDef gain) {
     if (channel > 3) {
         return 0;
     }
@@ -58,3 +55,80 @@ uint16_t ADS1015_SingleEnded(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress,
     // Shift 12-bit results right 4 bits for the ADS1015
     return ADS1015_readRegister(hi2c, i2cAddress, ADS1015_REG_POINTER_CONVERT) >> 4;
 }
+
+int16_t ADS1015_Differential_0_1(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress, Gain_TypeDef gain) {
+    // Start with default values
+    uint16_t config = ADS1015_REG_CONFIG_CQUE_NONE | // Disable the comparator (default val)
+            ADS1015_REG_CONFIG_CLAT_NONLAT | // Non-latching (default val)
+            ADS1015_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+            ADS1015_REG_CONFIG_CMODE_TRAD | // Traditional comparator (default val)
+            ADS1015_REG_CONFIG_DR_1600SPS | // 1600 samples per second (default)
+            ADS1015_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
+
+    // Set PGA/voltage range
+    config |= gain;
+
+    // Set channels
+    config |= ADS1015_REG_CONFIG_MUX_DIFF_0_1; // AIN0 = P, AIN1 = N
+
+    // Set 'start single-conversion' bit
+    config |= ADS1015_REG_CONFIG_OS_SINGLE;
+
+    // Write config register to the ADC
+    ADS1015_writeRegister(hi2c, i2cAddress, ADS1015_REG_POINTER_CONFIG, config);
+
+    // Wait for the conversion to complete
+    HAL_Delay(ADS1015_CONVERSIONDELAY);
+
+    // Read the conversion results
+    uint16_t res = ADS1015_readRegister(hi2c, i2cAddress,
+    ADS1015_REG_POINTER_CONVERT) >> 4;
+
+    // Shift 12-bit results right 4 bits for the ADS1015,
+    // making sure we keep the sign bit intact
+    if (res > 0x07FF) {
+        // negative number - extend the sign to 16th bit
+        res |= 0xF000;
+    }
+    return (int16_t) res;
+
+}
+
+int16_t ADS1015_Differential_2_3(I2C_HandleTypeDef* hi2c, uint8_t i2cAddress, Gain_TypeDef gain) {
+    // Start with default values
+    uint16_t config = ADS1015_REG_CONFIG_CQUE_NONE | // Disable the comparator (default val)
+            ADS1015_REG_CONFIG_CLAT_NONLAT | // Non-latching (default val)
+            ADS1015_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+            ADS1015_REG_CONFIG_CMODE_TRAD | // Traditional comparator (default val)
+            ADS1015_REG_CONFIG_DR_1600SPS | // 1600 samples per second (default)
+            ADS1015_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
+
+    // Set PGA/voltage range
+    config |= gain;
+
+    // Set channels
+    config |= ADS1015_REG_CONFIG_MUX_DIFF_2_3;          // AIN2 = P, AIN3 = N
+
+    // Set 'start single-conversion' bit
+    config |= ADS1015_REG_CONFIG_OS_SINGLE;
+
+    // Write config register to the ADC
+    ADS1015_writeRegister(hi2c, i2cAddress, ADS1015_REG_POINTER_CONFIG, config);
+
+    // Wait for the conversion to complete
+    HAL_Delay(ADS1015_CONVERSIONDELAY);
+
+    // Read the conversion results
+    uint16_t res = ADS1015_readRegister(hi2c, i2cAddress,
+    ADS1015_REG_POINTER_CONVERT) >> 4;
+
+    // Shift 12-bit results right 4 bits for the ADS1015,
+    // making sure we keep the sign bit intact
+    if (res > 0x07FF) {
+        // negative number - extend the sign to 16th bit
+        res |= 0xF000;
+    }
+    return (int16_t) res;
+
+}
+
