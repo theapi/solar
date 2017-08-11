243734c62ab7d712b39d916a514ce3257593d394
Peter Clarke <petermclarke@gmail.com>
2017-08-11 14:21:25 +0100

    Some defines

--- Mk3/stm32/lora_sensor/Inc/bh1750.h
+++ Mk3/stm32/lora_sensor/Inc/bh1750.h
@@ -13,8 +13,61 @@
 #include "main.h"
 #include "stdint.h"
 
-/* I2C ADDRESS/BITS */
+/*
+ * I2C ADDRESS/BITS
+ */
 #define BH1750_ADDRESS  (0x23 << 1) // 7 bit address. ADDR pin to GND
+
+/*
+ * No active state.
+ */
+#define BH1750_INS_POWER_DOWN 0x00
+
+/*
+ * Waiting for measurement command.
+ */
+#define BH1750_INS_POWER_ON 0x01
+
+/*
+ * Reset Data register value.
+ * Reset command is not acceptable in Power Down mode.
+ */
+#define BH1750_INS_RESET 0x07
+
+/* Start measurement at 1lx resolution.
+ * Measurement time is approx 120ms.
+ */
+#define BH1750_MODE_CONTINUOUS_HIGH_RES  0x10
+
+/* Start measurement at 0.5lx resolution.
+ * Measurement Time is typically 120ms.
+ */
+#define BH1750_MODE_CONTINUOUS_HIGH_RES_2 0x11
+
+/* Start measurement at 4lx resolution.
+ * Measurement time is approx 16ms.
+ */
+#define BH1750_MODE_CONTINUOUS_LOW_RES  0x13
+
+/* Start measurement at 1lx resolution.
+ * Measurement Time is typically 120ms.
+ * It is automatically set to Power Down mode after measurement.
+ */
+#define BH1750_MODE_ONE_TIME_HIGH_RES  0x20
+
+/*
+ * Start measurement at 0.5lx resolution.
+ * Measurement Time is typically 120ms.
+ * It is automatically set to Power Down mode after measurement.
+ */
+#define BH1750_MODE_ONE_TIME_HIGH_RES_2  0x21
+
+/*
+ * Start measurement at 4lx resolution.
+ * Measurement Time is typically 16ms.
+ * It is automatically set to Power Down mode after measurement.
+ */
+#define BH1750_MODE_ONE_TIME_LOW_RES  0x23
 
 
 
