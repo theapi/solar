1982702e3a1989929d0d673b95dd3d4e4958f7b1
Peter Clarke <peter_clarke@dennis.co.uk>
2018-10-01 13:31:58 +0100

    Elasticsearch logger

---
+++ datalogger/elasticsearch.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Listen for the broadcast UDP for the binary GardenPayload and send it to Elasticsearch.
+ *
+ * @see /src/GardenPayload.h
+ */
+
+use Elasticsearch\ClientBuilder;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+if (!($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP))) {
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+
+$binded = socket_bind($socket, '0.0.0.0', 12345);
+$rval = socket_set_option($socket, getprotobyname("ip"), MCAST_JOIN_GROUP,
+    array("group" => "239.0.0.57", "interface" => 0));
+
+
+// Build the elasticsearch client.
+$client = ClientBuilder::create()
+  ->setHosts(['192.168.0.2:9200']) // @todo configurable hosts.
+  ->build();
+
+// Only handle each message once, so start with fake one.
+$msg_id = 500;
+
+while (1) {
+    $from = '';
+    $port = 0;
+    $num = socket_recvfrom($socket, $buf, 53, MSG_WAITALL, $from, $port);
+    //echo "Received $num bytes from $from, port $port" . PHP_EOL;
+    //echo "UDP packet contents: $buf" . PHP_EOL;
+
+    if ($buf[0] == "\t") {
+        // A payload.
+        $binarydata = trim($buf);
+        if (strlen($binarydata) > 1) {
+            $array = unpack("Cdevice_id/Cmsg_id/n*", $binarydata);
+            foreach ($array as $k => $v) {
+                // There is no option "signed short (always 16 bit, big endian byte order)"
+                if (is_int($k)) {
+                    // Unpacked in the unsigned form,
+                    // and then if the result is >= 2^15, subtract 2^16 from it.
+                    if ($v >= pow(2, 15)) $v -= pow(2, 16);
+                    $array[$k] = $v;
+                }
+            }
+
+            if ($array['device_id'] == 50 && $array['msg_id'] != $msg_id) {
+              $msg_id = $array['msg_id'];
+              print_r($array);
+
+              // Prepare the doc.
+              $doc['device_id'] = $array['device_id'];
+              $doc['msg_id'] = $array['msg_id'];
+              $doc['vcc'] = $array[2];
+              $doc['mv'] = $array[3];
+              $doc['ma'] = $array[4];
+              $doc['light'] = $array[5];
+              $doc['cpu_temperature'] = $array[6];
+              // Convert the temperature to the float value.
+              $doc['temperature'] = $array[7] / 10;
+              // Record a timestamp as milliseconds since epoch (epoch_millis).
+              $doc['timestamp'] = date("U") * 1000;
+              print_r($doc);
+
+              $params = [
+                'index' => 'garden_payload',
+                'type' => '_doc',
+                'body' => $doc,
+              ];
+              $response = $client->index($params);
+              print_r($response);
+            }
+        }
+    }
+
+}
