3181e1ca345d6f9c9571f6e417fe3cf356bf9fd2
Peter Clarke <petermclarke@gmail.com>
2018-10-28 18:07:52 +0000

    Remove the tab from the UDP message.

--- datalogger/nodejs/SolarPayloadHandler.js
+++ datalogger/nodejs/SolarPayloadHandler.js
@@ -7,38 +7,42 @@ module.exports = class SolarPayloadHandler {
     this.msg_id = 999;
     this.client = client;
 
-    socket.on('message', (buf, rinfo) => {
-      if (buf.readUInt8(0) === 9 && buf.readUInt8(1) === 55) {
-        if (this.msg_id !== buf.readUInt8(3)) {
-          let payload = this.unserialize(buf);
-          payload.timestamp = new Date().getTime();
-          console.log(payload);
+    socket.on('message', (input, rinfo) => {
+      if (input.readUInt8(0) === 9) {
+        // Remove the tab from the UDP message.
+        const buf = input.slice(1);
+        if (buf.readUInt8(0) === 55) {
+          if (this.msg_id !== buf.readUInt8(2)) {
+            let payload = this.unserialize(buf);
+            payload.timestamp = new Date().getTime();
+            console.log(payload);
 
-          // Keep a record in a file.
-          let now = new Date();
-          let month = now.getUTCMonth() + 1;
-          let dir = __dirname + '/log/solar/' + now.getUTCFullYear();
-          // {recursive: true} requires > 10.12.0
-          fsPromises.mkdir(dir, {recursive: true})
-          .then(() => {
-            return fsPromises.appendFile(
-              dir + '/' + month + '.json',
-              JSON.stringify(payload) + "\n"
-            );
-          })
-          .catch(console.error);
+            // Keep a record in a file.
+            let now = new Date();
+            let month = now.getUTCMonth() + 1;
+            let dir = __dirname + '/log/solar/' + now.getUTCFullYear();
+            // {recursive: true} requires > 10.12.0
+            fsPromises.mkdir(dir, {recursive: true})
+            .then(() => {
+              return fsPromises.appendFile(
+                dir + '/' + month + '.json',
+                JSON.stringify(payload) + "\n"
+              );
+            })
+            .catch(console.error);
 
-          // Index into Elasticsearch.
-          this.client.index({
-            index: 'solar_0', //@todo not hard coded index name
-            type: '_doc',
-            body: payload
-          })
-          .catch ((err) => {
-            console.error('failed to index: ' + err);
-          })
-          ;
+            // Index into Elasticsearch.
+            this.client.index({
+              index: 'solar_0', //@todo not hard coded index name
+              type: '_doc',
+              body: payload
+            })
+            .catch ((err) => {
+              console.error('failed to index: ' + err);
+            })
+            ;
 
+          }
         }
       }
     });
@@ -46,20 +50,20 @@ module.exports = class SolarPayloadHandler {
 
   unserialize(buf) {
     let payload = {};
-    payload.msg_type = buf.readUInt8(1);
-    payload.device_id = buf.readUInt8(2);
-    payload.msg_id = buf.readUInt8(3);
+    payload.msg_type = buf.readUInt8(0);
+    payload.device_id = buf.readUInt8(1);
+    payload.msg_id = buf.readUInt8(2);
     this.msg_id = payload.msg_id;
-    payload.flags = buf.readUInt8(4);
-    payload.vcc = buf.readInt16BE(5);
-    payload.mv = buf.readInt16BE(7);
-    payload.ma = buf.readInt16BE(9);
-    payload.light = buf.readInt16BE(11);
-    payload.cpu_temperature = buf.readInt16BE(13);
-    payload.temperature = buf.readInt16BE(15) / 10;
-    payload.rssi = buf.readInt16BE(17);
-    payload.snr = buf.readInt16BE(19);
-    payload.freg_error = buf.readInt16BE(21);
+    payload.flags = buf.readUInt8(3);
+    payload.vcc = buf.readInt16BE(4);
+    payload.mv = buf.readInt16BE(6);
+    payload.ma = buf.readInt16BE(8);
+    payload.light = buf.readInt16BE(10);
+    payload.cpu_temperature = buf.readInt16BE(11);
+    payload.temperature = buf.readInt16BE(14) / 10;
+    payload.rssi = buf.readInt16BE(16);
+    payload.snr = buf.readInt16BE(18);
+    payload.freg_error = buf.readInt16BE(20);
 
     return payload;
   }
