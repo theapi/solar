f27bdd85bc11b425502b6199f2538a1a6eac9a09
Peter Clarke <petermclarke@gmail.com>
2018-10-30 20:34:19 +0000

    Config

--- datalogger/nodejs/app.js
+++ datalogger/nodejs/app.js
@@ -2,16 +2,19 @@
 const PORT = 12345;
 const MULTICAST_ADDR = "239.0.0.57";
 
+const config = require('config');
+const udp = config.get('udp');
+
 const SolarPayloadHandler = require('./SolarPayloadHandler.js');
 const GardenPayloadHandler = require('./GardenPayloadHandler.js');
 const client = require('./elasticsearch/connection.js');
 const dgram = require('dgram');
 const socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
 
-socket.bind(PORT);
+socket.bind(udp.port);
 
 socket.on("listening", function() {
-  socket.addMembership(MULTICAST_ADDR);
+  socket.addMembership(udp.address);
   const address = socket.address();
   console.log(
     `UDP socket listening on ${address.address}:${address.port} pid: ${
@@ -20,8 +23,8 @@ socket.on("listening", function() {
   );
 });
 
-let garden = new GardenPayloadHandler(socket);
-let solar = new SolarPayloadHandler(socket, client);
+//let garden = new GardenPayloadHandler(socket);
+//let solar = new SolarPayloadHandler(socket, client);
 
 // Just to show the serializer working :)
 //console.log(solar.unserialize(Buffer.from([0x09, 0x37, 0x02, 0x03, 0x04, 0x05, 0x06])));
